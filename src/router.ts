f7b26767cfa69d6ba4ce6dd15edb1cfee308c9b3
Markus <endzeit@gmail.com>
2019-11-29 22:05:39 +0100

    feat: Add API docs

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,41 @@ import { Key as TokenKey, pathToRegexp, TokensToRegexpOptions } from 'path-to-re
 // https://basarat.gitbooks.io/typescript/docs/tips/barrel.html
 export { pathToRegexp }
 
+/** Valid HTTP methods for matching. */
+export type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD' | 'OPTIONS'
+export type MethodWildcard = 'ALL'
+
+export interface Params {
+  [key: string]: string
+}
+
+/**
+ * Optional route options.
+ *
+ * @example
+ * // When `true` the regexp will be case sensitive. (default: `false`)
+ * sensitive?: boolean;
+ *
+ * // When `true` the regexp allows an optional trailing delimiter to match. (default: `false`)
+ * strict?: boolean;
+ *
+ * // When `true` the regexp will match to the end of the string. (default: `true`)
+ * end?: boolean;
+ *
+ * // When `true` the regexp will match from the beginning of the string. (default: `true`)
+ * start?: boolean;
+ *
+ * // Sets the final character for non-ending optimistic matches. (default: `/`)
+ * delimiter?: string;
+ *
+ * // List of characters that can also be "end" characters.
+ * endsWith?: string;
+ *
+ * // Encode path tokens for use in the `RegExp`.
+ * encode?: (value: string) => string;
+ */
+export interface RouteOptions extends TokensToRegexpOptions {}
+
 export interface Route<HandlerType> {
   method: Method | MethodWildcard
   path: string
@@ -12,51 +47,91 @@ export interface Route<HandlerType> {
   handler: HandlerType
 }
 
+/**
+ * The object returned when a route matches.
+ *
+ * The handler can then be used to execute the relevant function.
+ *
+ * @example
+ * {
+ *   params: Params
+ *   matches?: RegExpExecArray
+ *   method: Method | MethodWildcard
+ *   path: string
+ *   regexp: RegExp
+ *   options: RouteOptions
+ *   keys: Keys
+ *   handler: HandlerType
+ * }
+ */
 export interface RouteMatch<HandlerType> extends Route<HandlerType> {
   params: Params
   matches?: RegExpExecArray
 }
 
-export type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD' | 'OPTIONS'
-export type MethodWildcard = 'ALL'
-
-export interface Params {
-  [key: string]: string
-}
-
-export interface RouteOptions extends TokensToRegexpOptions {}
-
 export type Key = TokenKey
 export type Keys = Array<Key>
 
+/**
+ * Tiny request router. Allows overloading of handler type to be fully type safe.
+ *
+ * @example
+ * import { Router, Method, Params } from 'tiny-request-router'
+ *
+ * // Let the router know that handlers are async functions returning a Response
+ * type Handler = (params: Params) => Promise<Response>
+ *
+ * const router = new Router<Handler>()
+ */
 export class Router<HandlerType = any> {
+  /** List of all registered routes. */
   public routes: Array<Route<HandlerType>> = []
 
+  /** Add a route that matches any method. */
   public all(path: string, handler: HandlerType, options: RouteOptions = {}) {
     return this._push('ALL', path, handler, options)
   }
+  /** Add a route that matches the GET method. */
   public get(path: string, handler: HandlerType, options: RouteOptions = {}) {
     return this._push('GET', path, handler, options)
   }
+  /** Add a route that matches the POST method. */
   public post(path: string, handler: HandlerType, options: RouteOptions = {}) {
     return this._push('POST', path, handler, options)
   }
+  /** Add a route that matches the PUT method. */
   public put(path: string, handler: HandlerType, options: RouteOptions = {}) {
     return this._push('PUT', path, handler, options)
   }
+  /** Add a route that matches the PATCH method. */
   public patch(path: string, handler: HandlerType, options: RouteOptions = {}) {
     return this._push('PATCH', path, handler, options)
   }
+  /** Add a route that matches the DELETE method. */
   public delete(path: string, handler: HandlerType, options: RouteOptions = {}) {
     return this._push('DELETE', path, handler, options)
   }
+  /** Add a route that matches the HEAD method. */
   public head(path: string, handler: HandlerType, options: RouteOptions = {}) {
     return this._push('HEAD', path, handler, options)
   }
+  /** Add a route that matches the OPTIONS method. */
   public options(path: string, handler: HandlerType, options: RouteOptions = {}) {
     return this._push('OPTIONS', path, handler, options)
   }
-
+  /**
+   * Match the provided method and path against the list of registered routes.
+   *
+   * @example
+   * router.get('/foobar', async () => new Response('Hello'))
+   *
+   * const match = router.match('GET', '/foobar')
+   * if (match) {
+   *   // Call the async function of that match
+   *   const response = await match.handler()
+   *   console.log(response) // => Response('Hello')
+   * }
+   */
   public match(method: Method, path: string): RouteMatch<HandlerType> | null {
     for (const route of this.routes) {
       // Skip immediately if method doesn't match
